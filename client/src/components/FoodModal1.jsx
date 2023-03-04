33c8d8e7c3fbf65d1cd8f33aa48b233879986828
Milanka00 <milankatharangana01@gmail.com>
2023-03-04 10:47:36 +0530

    validation completed

--- client/src/components/FoodModal1.jsx
+++ client/src/components/FoodModal1.jsx
@@ -12,6 +12,9 @@ const FoodModal1 = () => {
             const [location, setLocation] = useState('');
             // const [preference, setPreference] = useState('comeToUs');
             const [errors, setErrors] = useState({});
+            // change the content
+            const [formSubmitted, setFormSubmitted] = useState(false);
+
 
           
             function handleSubmit(event) {
@@ -19,12 +22,18 @@ const FoodModal1 = () => {
               const newErrors = validateInputs();
               if (Object.keys(newErrors).length === 0) {
                 // submit form
+                //  event.target.reset();
+                setAddress('');
+                setDonationDate('');
+                setLocation('');
+                setFormSubmitted(true);
                 console.log('Form submitted successfully!');
-
+               
                 
               } else {
                 setErrors(newErrors);
               }
+             
             }
           
             function validateInputs() {
@@ -95,64 +104,74 @@ const FoodModal1 = () => {
                             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                          <div class="modal-body ">
-                         <p className='text-center'>Fill out this form to inform us about your food donations.</p>
+                         {formSubmitted ? (
+                              <p>Thank you for submitting the form!</p>
+                            ) : (
+                              <React.Fragment>
+                                <p className='text-center'>Fill out this form to inform us about your food donations.</p>
 
-<form onSubmit={handleSubmit}>
+                                <form onSubmit={handleSubmit}>
 
-            <div class="form-floating mb-3">
-                <input type="text" class="form-control" id="floatingInput" placeholder="address" 
-                value={address}
-                onChange={(e) => setAddress(e.target.value)}/>
-                <label for="floatingInput" style={{ color: "black" }}>Address</label>
-                {errors.address && <span className="error">{errors.address}</span>}
-            </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="text" class="form-control" id="floatingInput" placeholder="address" 
+                                        value={address}
+                                        onChange={(e) => setAddress(e.target.value)}/>
+                                        <label for="floatingInput" style={{ color: "black" }}>Address</label>
+                                        {errors.address && <span className="error">{errors.address}</span>}
+                                    </div>
 
-            <div class="form-floating mb-3">
-                <input type="date" class="form-control" id="floatingInput" placeholder="" 
-                value={donationDate}
-                onChange={(e) => setDonationDate(e.target.value)}/>
-                <label for="floatingInput" style={{ color: "black" }}>Donation Date</label>
-                {errors.donationDate && <span className="error">{errors.donationDate}</span>}
-            </div>
+                                    <div class="form-floating mb-3">
+                                        <input type="date" class="form-control" id="floatingInput" placeholder="" 
+                                        value={donationDate}
+                                        onChange={(e) => setDonationDate(e.target.value)}/>
+                                        <label for="floatingInput" style={{ color: "black" }}>Donation Date</label>
+                                        {errors.donationDate && <span className="error">{errors.donationDate}</span>}
+                                    </div>
 
-            <div class="form-floating mb-3">
+                                    <div class="form-floating mb-3">
 
-                <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="location"
-                value={location}
-                onChange={(e)=>setLocation(e.target.value)} />
-                <label for="exampleDataList" class="form-label" style={{ color: "black" }}>Nearest Location</label>
-                {errors.location && <span className='error'>{errors.location}</span>}
-                <datalist id="datalistOptions">
-                <option value="Galle" />
-                <option value="Matara" />
-                <option value="Ambalangoda" />
-                <option value="Welligama" />
-                <option value="Hambantota" />
-                </datalist>
-            </div>  
+                                        <input class="form-control" list="datalistOptions" id="exampleDataList" placeholder="location"
+                                        value={location}
+                                        onChange={(e)=>setLocation(e.target.value)} />
+                                        <label for="exampleDataList" class="form-label" style={{ color: "black" }}>Nearest Location</label>
+                                        {errors.location && <span className='error'>{errors.location}</span>}
+                                        <datalist id="datalistOptions">
+                                        <option value="Galle" />
+                                        <option value="Matara" />
+                                        <option value="Ambalangoda" />
+                                        <option value="Welligama" />
+                                        <option value="Hambantota" />
+                                        </datalist>
+                                    </div>  
 
-            <p>To continue with the food donation you have two options, either you can bring them to us or we can come and 
-                collect them. Please select your preference.</p>  
-            <div className='roption d-flex flex-column align-items-center  '>
-                <div class="form-check bg-color text-dark  mb-2  ps-4 p-2  rounded ">
-                    <input class="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked/>
-                    <label class="form-check-label " for="flexRadioDefault1">
-                        I will come and Donate to You
-                    </label>
-                    </div>
-                    <div class="form-check bg-color text-dark   mb-2 ps-4 p-2  rounded">
-                    <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
-                    <label class="form-check-label" for="flexRadioDefault2">
-                    You Have To Collect My Donation
-                    </label>
-                </div>
-                </div>
-                <p className='text-center mt-1'>Click here to submit your details.</p>
-            <div className='d-flex justify-content-center'>
-            <button className="btn text-white btncolor  ps-5 pe-5 " type='submit'>
-            Submit </button>
-            </div>
-            </form>
+                                    <p>To continue with the food donation you have two options, either you can bring them to us or we can come and 
+                                        collect them. Please select your preference.</p>  
+                                    <div className='roption d-flex flex-column align-items-center  '>
+                                        <div class="form-check bg-color text-dark  mb-2  ps-4 p-2  rounded ">
+                                            <input class="form-check-input " type="radio" name="flexRadioDefault" id="flexRadioDefault1" checked/>
+                                            <label class="form-check-label " for="flexRadioDefault1">
+                                                I will come and Donate to You
+                                            </label>
+                                            </div>
+                                            <div class="form-check bg-color text-dark   mb-2 ps-4 p-2  rounded">
+                                            <input class="form-check-input" type="radio" name="flexRadioDefault" id="flexRadioDefault2" />
+                                            <label class="form-check-label" for="flexRadioDefault2">
+                                            You Have To Collect My Donation
+                                            </label>
+                                        </div>
+                                        </div>
+                                        <p className='text-center mt-1'>Click here to submit your details.</p>
+                                    <div className='d-flex justify-content-center'>
+                                    <button className="btn text-white btncolor  ps-5 pe-5 " type='submit'>
+                                    Submit </button>
+                                    </div>
+                                  </form>
+                              </React.Fragment>
+
+
+                            )}
+
+                         
                         </div>
 
         
