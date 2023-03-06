f893b08bb64170cf6b5ce04d595fe4caf332cac2
Milanka00 <milankatharangana01@gmail.com>
2023-03-06 15:27:21 +0530

    css fixed

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,12 +1,24 @@
 import React from 'react';
 import './Login.css';
-// import image301 from '../images/image301.jpg';
+import Nav from './navBar';
+import {motion} from 'framer-motion';
+
 
 const Login = () => {
     return ( 
-        <React.Fragment>
-           
-        <div class="form-box">
+    
+
+<React.Fragment>
+                    <motion.div
+                     initial={{width:0}}
+                     animate={{width:"100vw"}}
+                     exit={{x:window.innerWidth, transition:{duration:0.1}}}
+        >
+        <div className="bg-image-login">
+          
+        <Nav/>
+       <div className='form-container mt-5 m-1'>
+        <div class="form-box1">
             <div class="form-value">
                 <form action="">
                     <h2>Login</h2>
@@ -20,18 +32,22 @@ const Login = () => {
                         <input type="password" required />
                         <label for="">Password</label>
                     </div>
-                    <div class="forget">
-                        <label for=""><input type="checkbox" />Remember Me  <a href="#">Forget Password</a></label>
+                    <div class="forget d-flex justify-content-between mt-1">
+                       
+                        <label for=""><input type="checkbox" />Remember Me</label>
+                        <p><a className='text-white' href="#"><p>Forget Password</p></a></p>
                       
                     </div>
-                    <button>Log in</button>
+                    <button className='button1'>Log in</button>
                     <div class="register">
-                        <p>Don't have a account? <a href="#">SignUp</a></p>
+                        <p>Don't have a account? <a href='/signup'>SignUp</a></p>
                     </div>
                 </form>
             </div>
         </div>
- 
+        </div>
+        </div>
+        </motion.div>
         </React.Fragment>
      );
 }
