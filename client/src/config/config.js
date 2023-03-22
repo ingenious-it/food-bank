fdb8fe22b7f6e1ebe8f8bc1c91b91ffd7b557602
tha-ree-jan <thareejanp.20@uom.lk>
2023-03-23 00:32:40 +0530

    paypal integration

---
+++ client/src/config/config.js
@@ -0,0 +1,6 @@
+export const CLIENT_ID =
+  process.env.CLIENT_ID ||
+  "AYMC0ekMQPv61h2yWXxG5wtHv32tj2GBiYiLgN7Ew3XLNNJ0BVa_gVFC5JKyTmYusvoDPmR9szLS-Uxd";
+export const APP_SECRET =
+  process.env.APP_SECRET ||
+  "EIeBKVA3DhI0koMf_gNbqcZ0B5-l5aWY-IQ4X7x_uT2scgPphrK0JlVZSy6YwAeYWJGz0ErB-XUyR34X";
