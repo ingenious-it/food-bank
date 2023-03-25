c3af7512c653ba7f108b9da7105bd40143db83a4
Amal Shalinda <Shalindajda99@gmail.com>
2023-03-25 16:57:13 +0530

    no changes

--- server/src/main/java/com/example/server/entities/FoodDonation.java
+++ server/src/main/java/com/example/server/entities/FoodDonation.java
@@ -1,13 +1,17 @@
 package com.example.server.entities;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.Date;
 
 @Entity
 @Table(name="food_donation")
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class FoodDonation {
 
     @Id
@@ -16,5 +20,5 @@ public class FoodDonation {
     private String DonationAddress;
     private Date DonationDate;
     private String NearestLocation;
-    private String Option;
+    private String SelectedOption;
 }
