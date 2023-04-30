90436655ed2d42d6b1f87f444cb15b1b113e673a
Amal Shalinda <Shalindajda99@gmail.com>
2023-04-30 12:00:04 +0530

    emailvalidation

--- client/src/components/ForgetPassword.jsx
+++ client/src/components/ForgetPassword.jsx
@@ -1,9 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ForgetPassword.css";
 import Nav from "./navBar";
 import { motion } from "framer-motion";
 
 const ForgetPassword = () => {
+  const [email, setEmail] = useState("");
+  const [isEmailValid, setIsEmailValid] = useState(false);
+
+  const handleEmailChange = (event) => {
+    const value = event.target.value;
+    setEmail(value);
+
+    // Check if the email is valid using a regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    setIsEmailValid(emailRegex.test(value));
+  };
+
   return (
     <React.Fragment>
       <motion.div
@@ -21,12 +33,19 @@ const ForgetPassword = () => {
                   <p>We will send you reset instructions</p>
                   <div class="inputbox">
                     {/* <ion-icon name="mail-outline"></ion-icon> */}
-                    <input type="Username" required />
+                    <input
+                      type="email"
+                      required
+                      value={email}
+                      onChange={handleEmailChange}
+                    />
 
                     <label for="">Email Address</label>
                   </div>
 
-                  <button className="button1">Reset Password</button>
+                  <button className="button1" disabled={!isEmailValid}>
+                    Reset Password
+                  </button>
                 </form>
               </div>
             </div>
