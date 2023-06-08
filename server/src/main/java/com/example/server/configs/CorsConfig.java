559bec3e09f709f0d6d4817aae1c2ffd30db21e7
tha-ree-jan <thareejap.20@uom.lk>
2023-06-08 19:12:25 +0530

    jwt authorization success

--- server/src/main/java/com/example/server/configs/CorsConfig.java
+++ server/src/main/java/com/example/server/configs/CorsConfig.java
@@ -5,22 +5,34 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-@Configuration
-public class CorsConfig {
+//@Configuration
+//public class CorsConfig {
+    @Configuration
+    public class CorsConfig implements WebMvcConfigurer {
 
-
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:8080","http://localhost:3001")
-                        .allowedMethods("GET", "POST", "PUT", "DELETE")
-                        .allowedHeaders("*")
-                        .allowCredentials(true)
-                        .maxAge(3600);
-            }
-        };
+        @Override
+        public void addCorsMappings(CorsRegistry registry) {
+            registry.addMapping("/**")
+                    .allowedOrigins("http://localhost:3000", "http://localhost:3001") // Add your frontend origins here
+                    .allowedMethods("GET", "POST", "PUT", "DELETE") // Add the HTTP methods you want to allow
+                    .allowedHeaders("*") // Allow all headers
+                    .allowCredentials(true); // Allow sending of credentials (e.g., cookies, authorization headers)
+        }
     }
-}
+
+
+//    public WebMvcConfigurer corsConfigurer() {
+//        return new WebMvcConfigurer() {
+//            @Override
+//            public void addCorsMappings(CorsRegistry registry) {
+//                registry.addMapping("/**")
+//                        .allowedOrigins("http://localhost:3000", "http://localhost:8080","http://localhost:3001")
+//                        .allowedMethods("GET", "POST", "PUT", "DELETE")
+//                        .allowedHeaders("*")
+//                        .allowCredentials(true)
+//                        .maxAge(3600);
+//            }
+//        };
+//    }
+//}
 
--- server/src/main/java/com/example/server/services/AdminUserService.java
+++ server/src/main/java/com/example/server/services/AdminUserService.java
@@ -1,6 +1,8 @@
 package com.example.server.services;
 
 import com.example.server.entities.AdminUser;
+import com.example.server.loginConfig.JWTAuthentication;
+import com.example.server.loginConfig.LoginResponse;
 import com.example.server.repositories.AdminUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -12,11 +14,13 @@ import java.util.List;
 public class AdminUserService {
     private final AdminUserRepository adminUserRepository;
     private final BCryptPasswordEncoder passwordEncoder;
+    private final JWTAuthentication jwtAuthentication;
     @Autowired
 
-    public AdminUserService(AdminUserRepository adminUserRepository , BCryptPasswordEncoder passwordEncoder) {
+    public AdminUserService(AdminUserRepository adminUserRepository , BCryptPasswordEncoder passwordEncoder, JWTAuthentication jwtAuthentication) {
         this.adminUserRepository = adminUserRepository;
         this.passwordEncoder = passwordEncoder;
+        this.jwtAuthentication = jwtAuthentication;
     }
     public AdminUser saveAdminUser(AdminUser adminUser) {
         String encryptedPassword = passwordEncoder.encode(adminUser.getPassword());
@@ -30,4 +34,21 @@ public class AdminUserService {
         return adminUserRepository.findById(userName).orElse(null);
     }
 
+
+    public LoginResponse performlogin(String userName, String password) {
+        AdminUser adminUser = adminUserRepository.findById(userName).orElse(null);
+        System.out.println(adminUser);
+        if (adminUser == null) {
+            return new LoginResponse("User not found", false,null);
+        } else {
+            boolean isPasswordMatched = passwordEncoder.matches(password, adminUser.getPassword());
+            if (isPasswordMatched) {
+                String token= jwtAuthentication.generateToken(userName);
+                System.out.println(token);
+                return new LoginResponse("Login Successful", true,token);
+            } else {
+                return new LoginResponse("Incorrect Password", false,null);
+            }
+        }
+    }
 }
