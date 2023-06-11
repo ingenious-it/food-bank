bd67229f5ff29b2a91d75d6ad0833eea28220d3e
tha-ree-jan <thareejap.20@uom.lk>
2023-06-11 06:58:57 +0530

    merged with createUser

--- server/src/main/java/com/example/server/loginconfig/LoginResponse.java
+++ server/src/main/java/com/example/server/loginconfig/LoginResponse.java
@@ -1,19 +1,35 @@
-package com.example.server.loginconfig;
+package com.example.server.loginConfig;
 
 public class LoginResponse {
-        private String message;
-        private String token;
-
-
-    public LoginResponse(String message, String token) {
-        this.message=message;
+    private String message;
+    private boolean isLoginSuccess;
+    private String token;
+    public LoginResponse(String message, boolean isLoginSuccess,String token) {
         this.token=token;
+        this.message = message;
+        this.isLoginSuccess = isLoginSuccess;
+    }
+    public String getMessage() {
+        return message;
+    }
+    public boolean isLoginSuccess() {
+        return isLoginSuccess;
+    }
 
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void setLoginSuccess(boolean loginSuccess) {
+        isLoginSuccess = loginSuccess;
     }
 
     public String getToken() {
         return token;
     }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
 }
-
-
