29d65409e077648caac9b32c03aad45f75253be3
Amal Shalinda <Shalindajda99@gmail.com>
2023-06-11 08:04:21 +0530

    warehouse 14

--- server/src/main/java/com/example/server/services/FoodItemService.java
+++ server/src/main/java/com/example/server/services/FoodItemService.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
 
 @Service
 public class FoodItemService {
@@ -18,6 +20,29 @@ public class FoodItemService {
     public FoodItem saveIssued(FoodItem foodItem)
     {
         return foodItemRepository.save(foodItem);
+    }
+
+    public FoodItem updateQuantityById(int id, int newQuantity) {
+        Optional<FoodItem> optionalFoodItem = foodItemRepository.findById(id);
+
+        if (optionalFoodItem.isPresent()) {
+            FoodItem foodItem = optionalFoodItem.get();
+            foodItem.setQuantity(newQuantity);
+            return foodItemRepository.save(foodItem);
+        } else {
+            throw new NoSuchElementException("FoodItem with ID " + id + " not found");
+        }
+    }
+
+    public int getQuantityById(int id) {
+        Optional<FoodItem> optionalFoodItem = foodItemRepository.findById(id);
+
+        if (optionalFoodItem.isPresent()) {
+            FoodItem foodItem = optionalFoodItem.get();
+            return foodItem.getQuantity();
+        } else {
+            throw new NoSuchElementException("FoodItem with ID " + id + " not found");
+        }
     }
 
 
